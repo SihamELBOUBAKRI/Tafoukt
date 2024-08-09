90b0a8601eb937344f0ffd93966cf0f53159f898
SihamELBOUBAKRI <155733015+SihamELBOUBAKRI@users.noreply.github.com>
2024-08-09 13:50:44 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SiHwasal</title>
+    <link rel="stylesheet" href="stile.css">    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+    
+</head>
+<body>
+<header>
+    <nav>
+        <img src="" alt="">
+        <a href="#home">Home</a>
+        <a href="#aboutUs">About Us</a>
+        <a href="#products">Our Product</a>
+        <a href="#location">Location</a>
+        <a href="#contact">Contact</a>
+
+    </nav>
+    <h1 class="title"> SIHWASAL</h1>
+    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit.</P>
+
+
+
+</header>
+
+<main></main>
+
+
+<script src="main.js"></script>    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>
